d06031086801aa82e47e7f810ed8430c10344fc1
gokdenizozkan <gokdeniz@gokdenizozkan.com>
2026-01-13 18:18:19 +0300

    add 2015 puzzle 02-1

---
+++ 2015/puzzle_02-1.exs
@@ -0,0 +1,29 @@
+defmodule RectangularCuboid do
+  def new([w, h, l]) do
+    {elem(Integer.parse(w), 0), elem(Integer.parse(h), 0), elem(Integer.parse(l), 0)}
+  end
+  def new(l, w, h) do
+    {l, w, h}
+  end
+
+  def surface_area({l, w, h}) do
+    2*(l*w + l*h + w*h)
+  end
+
+  def surface_area_of_smallest_side({l, w, h}) do
+    Enum.min([l * w, l * h, w * h])
+  end
+
+  def valid?({l, w, h}) do
+    is_integer(l) and is_integer(w) and is_integer(h)
+  end
+end
+
+{:ok, input} = File.read("./puzzle_02_input.txt")
+boxes = input
+        |> String.split("\r\n", trim: true)
+        |> Enum.map(fn content -> String.split(content, "x") end)
+        |> Enum.map(&RectangularCuboid.new/1)
+
+result = Enum.reduce(boxes, 0, fn box, acc -> acc + RectangularCuboid.surface_area(box) + RectangularCuboid.surface_area_of_smallest_side(box) end)
+IO.inspect(result)
